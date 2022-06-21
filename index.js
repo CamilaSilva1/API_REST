76f1d8d4442d1f435ac2649ec82c74679278be05
CamilaSilva <camiladasilvaassis1@gmail.com>
2022-06-21 18:30:40 -0300

    feature: criação do projeto

---
+++ index.js
@@ -0,0 +1,17 @@
+// Criado por Camila Silva
+// Desde 21/06/2022
+// Criando uma API Rest com Node.js e MongoDB
+
+// Configuração inicial
+// chamando os pacotes instalados
+const express = require('express');
+
+// criando uma variavel para executar o express e iniciar
+const app =  express();
+
+// lendo o json
+
+
+// rota inicial / endpoint
+
+// entregando uma porta para o express disponibilizar o banco para ser acessado 
