ef3e3d892ccd441e53ce9354195d13faeb565908
CamilaSilva <camiladasilvaassis1@gmail.com>
2022-06-22 18:32:14 -0300

    feature: criando a requisição e resposta teste e testando no postman

--- index.js
+++ index.js
@@ -9,9 +9,25 @@ const express = require('express');
 // criando uma variavel para executar o express e iniciar
 const app =  express();
 
-// lendo o json
+// lendo o json com middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
 
+app.use(express.json());
 
-// rota inicial / endpoint
+// rota inicial / criando endpoint
+app.get('/', (req, resp) => {
+    // mostrando a requisição
+    resp.json({
+        message: "Olá express"
+    })
+})
 
-// entregando uma porta para o express disponibilizar o banco para ser acessado 
+// entregando uma porta para o express disponibilizar o banco para ser 
+// acessado
+
+// deixando o express visivel para o navegador
+app.listen(3000)
