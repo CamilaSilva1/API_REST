22f12ee1c01ced6fcf1b90335bf898423b6b34f8
CamilaSilva <camiladasilvaassis1@gmail.com>
2022-07-05 16:30:02 -0300

    feature: finalização da aplicação

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// importando arquivos
+const routes = require('./config/routes.js');
+
 // iniciando servidor
 const app = express();
 
 // adicionando e configurando os pacotes ao app criado
 app.use(morgan('dev'));
-
 app.use(bodyParser.urlencoded({
     extended: false
 }))
-
 app.use(express.json());
-
 app.use(cors());
+app.use(routes);
+
+// ligando o servidor
+app.listen(21262, () => {
+    console.log(`Express started at http://localhost:21262`)
+})
+
